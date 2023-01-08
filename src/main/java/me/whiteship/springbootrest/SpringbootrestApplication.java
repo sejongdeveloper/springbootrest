d5a7ddd2742ced416c3949e61edb26b4e85a0db3
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-08 14:39:26 +0900

    스프링 REST 클라이언트 2부: 커스터마이징

--- src/main/java/me/whiteship/springbootrest/SpringbootrestApplication.java
+++ src/main/java/me/whiteship/springbootrest/SpringbootrestApplication.java
@@ -2,8 +2,11 @@ package me.whiteship.springbootrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
@@ -15,10 +18,15 @@ public class SpringbootrestApplication {
 
 	@Bean
 	public WebClientCustomizer webClientCustomizer() {
-		return new WebClientCustomizer() {
+		return webClientBuilder -> webClientBuilder.baseUrl("http://localhost:8080");
+	}
+
+	@Bean
+	public RestTemplateCustomizer restTemplateCustomizer() {
+		return new RestTemplateCustomizer() {
 			@Override
-			public void customize(WebClient.Builder webClientBuilder) {
-				webClientBuilder.baseUrl("http://localhost:8080");
+			public void customize(RestTemplate restTemplate) {
+				restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
 			}
 		};
 	}
